b2eed7ca7cbcbcffa3da981fe29f23795ec17469
okahontas0321 <okahontas0321@ezweb.ne.jp>
2019-09-17 21:59:32 +0900

    pull

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "chatspace"
 set :repo_url, "okahontas0321/chatspace.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
-set :rbenv_ruby, 'ruby 2.5.1'
+set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],
 keys: ['~/.ssh/okapon.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
